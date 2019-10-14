72e6e0e275e4510a69d7ac3999b7814f214683b9
Sean Eastin <s198013@students.aie.edu>
2019-10-14 17:27:47 -0500

    Add project files.

---
+++ interfaces/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    public interface Itestable
+    {
+        string print();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person person = new person();
+            item item = new item();
+            person.print();
+            item.print();
+            Console.ReadKey();
+        }
+    }
+    class person : Itestable
+    {
+
+        public string print()
+        {
+            Console.WriteLine("person");
+            return "person";
+        }
+    }
+    class item : Itestable
+    {
+
+        public string print()
+        {
+            Console.WriteLine("Item");
+            return "item";
+        }
+    }
+}
